f25a49b154b008ec03b66e12ebf999bf43b9e792
Rebecca Bland <rebeccaannebland@gmail.com>
2021-11-28 14:35:17 -0800

    dashbaord updates

--- app.py
+++ app.py
@@ -48,10 +48,17 @@ for i in sales.index:
                             showarrow=False))
 
 number_fig.update_layout(annotations=annotations)
+number_fig.update_layout(title='Product Category Purchase Ratio')
 number_fig.update_layout({'plot_bgcolor': 'rgba(0, 0, 0, 0)','paper_bgcolor': 'rgba(0, 0, 0, 0)'})
 number_fig.update_yaxes(showline=True, linewidth=2, linecolor='black')
 number_fig.update_xaxes(visible=False)
 
+number_fig.update_layout(
+    font=dict(
+        size=20,
+        color="DarkGreen"
+    )
+)
 #Product Sales vs Income
 conditions = [
     (data['Income'] <= 25000),
@@ -91,14 +98,20 @@ sales_income_fig.update_layout(
     )
 )
 sales_income_fig.update_layout({'plot_bgcolor': 'rgba(0, 0, 0, 0)','paper_bgcolor': 'rgba(0, 0, 0, 0)'})
+sales_income_fig.update_layout(
+    font=dict(
+        size=20,
+        color="goldenrod"
+    )
+)
 
 # Age vs Product Breakdown Treemap (Has Dropdown)
 # boomers 1946 - 1965
 # gen x 1965 - 1980
 # Millennials 1981 – 1996
 bins = [1939, 1965, 1980, 1996]
-name = ['Boomers', 'Gen x', 'Millennials']
-data['Gen'] = pd.cut(data['Year_Birth'], bins, labels=name)
+gen_name = ['Boomers', 'Gen x', 'Millennials']
+data['Gen'] = pd.cut(data['Year_Birth'], bins, labels=gen_name)
 data['Total_purchase'] = data['MntWines']+data['MntMeatProducts']+data['MntGoldProds']+data['MntFishProducts']+data['MntSweetProducts']+data['MntFruits']
 product_dropdown = [
             {'label': 'Wine', 'value': 'MntWines'},
@@ -112,37 +125,74 @@ product_dropdown = [
 age_product_fig = px.treemap(data, path=['Gen'], values='Total_purchase',color=data['Gen'],
                             title='Product Purchases by Age',color_discrete_map={'Boomers':'DarkGreen', 'Gen x':'wheat', 'Millennials':'DarkSeaGreen'})
 age_product_fig.update_layout({'plot_bgcolor': 'rgba(0, 0, 0, 0)','paper_bgcolor': 'rgba(0, 0, 0, 0)'})
-
+age_product_fig.update_layout(
+    font=dict(
+        size=20,
+        color="DarkGreen"
+    )
+)
 # Sales by Channel Divided by Age
 #Generation
-tidydf = data.melt( 
+tidydf_gen = data.melt( 
             id_vars = 'Gen',
             value_vars = ['NumWebPurchases','NumCatalogPurchases', 'NumStorePurchases'],
             var_name = 'channel', 
-            value_name = 'no. of sales')
-sales_by_channel_gen_fig = px.bar(tidydf, x="channel", y="no. of sales",
+            value_name = '# of Sales')
+tidydf_gen2= pd.DataFrame(tidydf_gen.groupby(['Gen','channel'])['# of Sales'].sum()).reset_index()
+sales_by_channel_gen_fig = px.bar(tidydf_gen2, x="channel", y="# of Sales",
              color='Gen',
              pattern_shape="Gen",
              color_discrete_map={'Boomers':'goldenrod', 'Gen x':'wheat', 'Millennials':'khaki'},
-             title='Sales by Channel and Generation')
+             title='Sales by Channel and Generation',
+             labels=dict(channel="# of Purchases per Channel"),
+             category_orders={'channel': ["Catalog ", "In Store", "Web/Online"]})
 #Age
 age_bins = [18, 30, 45, 85]
 age_name = ['18-30', '30-45','45-85']
 data['agebracket'] = pd.cut(data['Age'], age_bins, labels=age_name)
-tidydf1 = data.melt( 
+tidydf_age = data.melt( 
             id_vars = 'agebracket',
             value_vars = ['NumWebPurchases','NumCatalogPurchases', 'NumStorePurchases'],
             var_name = 'channel', 
-            value_name = 'no. of sales')
-sales_by_channel_age_fig = px.bar(tidydf1, x="channel", y="no. of sales",
+            value_name = '# of Sales')
+tidydf_age2= pd.DataFrame(tidydf_age.groupby(['agebracket','channel'])['# of Sales'].sum()).reset_index()
+sales_by_channel_age_fig = px.bar(tidydf_age2, x="channel", y="# of Sales",
              color='agebracket',
              pattern_shape="agebracket",
              color_discrete_map={'18-30':'goldenrod', '30-45':'wheat', '45-85':'khaki'},
-             title='Sales by Channel and Age')
+             title='Sales by Channel and Age',
+             labels=dict(channel="# of Purchases per Channel"))
 
 sales_by_channel_gen_fig.update_layout({'plot_bgcolor': 'rgba(0, 0, 0, 0)','paper_bgcolor': 'rgba(0, 0, 0, 0)'})
 sales_by_channel_age_fig.update_layout({'plot_bgcolor': 'rgba(0, 0, 0, 0)','paper_bgcolor': 'rgba(0, 0, 0, 0)'})
-
+sales_by_channel_gen_fig.update_layout(
+    font=dict(
+        size=20,
+        color="DarkGreen"
+    )
+)
+sales_by_channel_age_fig.update_layout(
+    font=dict(
+        size=20,
+        color="DarkGreen"
+    )
+)
+# Bubble Chart Fig
+bubble_chart_fig = go.Figure(data=[go.Scatter(
+    x=data['Country'], y=data['Total_purchase'],
+    mode='markers')
+])
+bubble_chart_fig = px.scatter_geo(data, locations="Country", color="Country",
+                     hover_name="Country", size="Total_purchase",
+                     projection="natural earth", title='Bubble Chart of Product Sales by Country',
+                     color_discrete_map={'Spain':'DarkGreen', 'USA':'DarkSeaGreen', 'India':'wheat', 'Saudi Arabia':'crimson', 'Canada':'goldenrod', 'Germany':'Tomato', 'Australia':'lightslategray', 'Mexico':'saddlebrown'})
+bubble_chart_fig.update_layout({'plot_bgcolor': 'rgba(0, 0, 0, 0)','paper_bgcolor': 'rgba(0, 0, 0, 0)'})
+bubble_chart_fig.update_layout(
+    font=dict(
+        size=20,
+        color="DarkGreen"
+    )
+)
 # **************************************************************************************************************
 
 # HTML Styles: https://www.w3schools.com/html/html_styles.asp
@@ -161,93 +211,92 @@ app.layout = html.Div(children=[
                         style={'textAlign': 'center','color': 'DarkGreen', 'fontSize': '44px'}, 
                         className="header-title"), 
                 html.H2(
-                    children="Sub-Header Dashboard Description",
+                    children="Analysis of Campaign Impact on Product Sales",
                     className="header-description", style={'textAlign': 'center','color': 'DarkSeaGreen', 'fontSize': '30px'},
                 ),
             ],
             className="header",style={'background':'whitesmoke'},
         ),
-    # NUMBER SUMMARY DIV
+    # SALES SUMMARY
     html.Div(children=[
+
+        # NUMBER SUMMARY
+        html.Div(children=[
 
         #TOP ROW
         html.Div(children=[
             #CARD #1
             html.Div(children=[
                 html.H3("Wine Sales", style={'textAlign':'center', "margin-bottom":"0px", 'color': "goldenrod", 'fontSize': '30px', 'width': '100%', 'display': 'inline-block'}),
-                # html.P(f'{data['column'].loc[]}'), style={'textAlign':'center', "margin-bottom":"0px", 'color': "darkseagreen", 'fontSize': '40px', 'width': '24%', 'display': 'inline-block'},
-                html.P(wines_sales, style={'textAlign':'center', "margin-bottom":"0px", 'color': "darkseagreen", 'fontSize': '40px', 'width': '100%'})
+                html.P(wines_sales, style={'textAlign':'center', "margin-bottom":"0px", "margin-top":"8px", 'color': "darkseagreen", 'fontSize': '45px', 'width': '100%'})
             ],style={'width':'23%','display': 'inline-block','background': 'wheat','textAlign':'center', 
                     'border-style':'solid','border-color':'wheat', 'border-radius':'20px','border-width':'5px',
-                    'margin-bottom':'15px','margin-top':'15px','margin-left':'15px'}),
+                    'margin-bottom':'15px','margin-top':'15px','margin-left':'15px', 'height':'70%'}),
             #CARD #2
             html.Div(children=[
                 html.H3("Fruit Sales", style={'textAlign':'center', "margin-bottom":"0px", 'color': "goldenrod", 'fontSize': '30px', 'width': '100%', 'display': 'inline-block'}),
-                html.P(fruits_sales, style={'textAlign':'center', "margin-bottom":"0px", 'color': "darkseagreen", 'fontSize': '40px', 'width': '100%','display': 'block'})
+                html.P(fruits_sales, style={'textAlign':'center', "margin-bottom":"0px", "margin-top":"8px", 'color': "darkseagreen", 'fontSize': '45px', 'width': '100%','display': 'block'})
             ],style={'width':'23%','display': 'inline-block','background': 'wheat','textAlign':'center', 
                     'border-style':'solid','border-color':'wheat', 'border-radius':'20px','border-width':'5px',
-                    'margin-bottom':'15px','margin-top':'15px','margin-left':'15px'}),
+                    'margin-bottom':'15px','margin-top':'15px','margin-left':'15px', 'height':'70%'}),
             #CARD #3
             html.Div(children=[
                 html.H3("Meat Sales", style={'textAlign':'center', "margin-bottom":"0px", 'color': "goldenrod", 'fontSize': '30px', 'width': '100%', 'display': 'inline-block'}),
-                html.P(meat_sales, style={'textAlign':'center', "margin-bottom":"0px", 'color': "darkseagreen", 'fontSize': '40px', 'width': '100%'})
+                html.P(meat_sales, style={'textAlign':'center', "margin-bottom":"0px", "margin-top":"8px", 'color': "darkseagreen", 'fontSize': '45px', 'width': '100%'})
             ],style={'width':'23%','display': 'inline-block','background': 'wheat','textAlign':'center', 
                     'border-style':'solid','border-color':'wheat', 'border-radius':'20px','border-width':'5px',
-                    'margin-bottom':'15px','margin-top':'15px','margin-left':'15px'}),
+                    'margin-bottom':'15px','margin-top':'15px','margin-left':'15px', 'height':'70%'}),
             #CARD #4 (Total Purchase Count)
             html.Div(children=[
                 html.H3("Purchase Count", style={'textAlign':'center', "margin-bottom":"0px", 'color': "goldenrod", 'fontSize': '30px', 'width': '100%', 'display': 'inline-block'}),
-                html.P(total_purchases, style={'textAlign':'center', "margin-bottom":"0px", 'color': "darkgreen", 'fontSize': '40px', 'width': '100%'})
+                html.P(total_purchases, style={'textAlign':'center', "margin-bottom":"0px", "margin-top":"8px", 'color': "darkgreen", 'fontSize': '45px', 'width': '100%'})
             ],style={'width':'23%','display': 'inline-block','background': 'wheat','textAlign':'center', 
                     'border-style':'solid','border-color':'wheat', 'border-radius':'20px','border-width':'5px',
-                    'margin-right':'15px','margin-bottom':'15px','margin-top':'15px','margin-left':'15px'})
-            ],style={'width':'66%','display': 'inline-block','background': 'whitesmoke'}),
+                    'margin-right':'15px','margin-bottom':'15px','margin-top':'15px','margin-left':'15px', 'height':'70%'})
+            ],style={'width':'100%','display': 'inline-block','background': 'whitesmoke', 'height':'200px'}),
         
         #BOTTOM ROW
         html.Div(children=[
             #CARD #5
             html.Div(children=[
                 html.H3("Fish Sales", style={'textAlign':'center', "margin-bottom":"0px", 'color': "goldenrod", 'fontSize': '30px', 'width': '100%', 'display': 'inline-block'}),
-                html.P(fish_sales, style={'textAlign':'center', "margin-bottom":"0px", 'color': "darkseagreen", 'fontSize': '40px', 'width': '100%'})
+                html.P(fish_sales, style={'textAlign':'center', "margin-bottom":"0px", "margin-top":"8px", 'color': "darkseagreen", 'fontSize': '45px', 'width': '100%'})
             ],style={'width':'23%','display': 'inline-block','background': 'wheat','textAlign':'center', 
                     'border-style':'solid','border-color':'wheat', 'border-radius':'20px','border-width':'5px',
-                    'margin-bottom':'15px','margin-top':'15px','margin-left':'15px'}),
+                    'margin-bottom':'15px','margin-top':'15px','margin-left':'15px', 'height':'70%', 'height':'70%'}),
             #CARD #6
             html.Div(children=[
                 html.H3("Sweets Sales", style={'textAlign':'center', "margin-bottom":"0px", 'color': "goldenrod", 'fontSize': '30px', 'width': '100%', 'display': 'inline-block'}),
-                html.P(sweets_sales, style={'textAlign':'center', "margin-bottom":"0px", 'color': "darkseagreen", 'fontSize': '40px', 'width': '100%'})
+                html.P(sweets_sales, style={'textAlign':'center', "margin-bottom":"0px", "margin-top":"8px", 'color': "darkseagreen", 'fontSize': '45px', 'width': '100%'})
             ],style={'width':'23%','display': 'inline-block','background': 'wheat','textAlign':'center', 
                     'border-style':'solid','border-color':'wheat', 'border-radius':'20px','border-width':'5px',
-                    'margin-bottom':'15px','margin-top':'15px','margin-left':'15px'}),
+                    'margin-bottom':'15px','margin-top':'15px','margin-left':'15px', 'height':'70%'}),
             #CARD #7
             html.Div(children=[
                 html.H3("Gold Sales", style={'textAlign':'center', "margin-bottom":"0px", 'color': "goldenrod", 'fontSize': '30px', 'width': '100%', 'display': 'inline-block'}),
-                html.P(gold_sales, style={'textAlign':'center', "margin-bottom":"0px", 'color': "darkseagreen", 'fontSize': '40px', 'width': '100%'})
+                html.P(gold_sales, style={'textAlign':'center', "margin-bottom":"0px", "margin-top":"8px", 'color': "darkseagreen", 'fontSize': '45px', 'width': '100%'})
             ],style={'width':'23%','display': 'inline-block','background': 'wheat','textAlign':'center', 
                     'border-style':'solid','border-color':'wheat', 'border-radius':'20px','border-width':'5px',
-                    'margin-bottom':'15px','margin-top':'15px','margin-left':'15px'}),
+                    'margin-bottom':'15px','margin-top':'15px','margin-left':'15px', 'height':'70%'}),
             #CARD #8 (Total Sales)
             html.Div(children=[
                 html.H3("Total Sales", style={'textAlign':'center', "margin-bottom":"0px", 'color': "goldenrod", 'fontSize': '30px', 'width': '100%', 'display': 'inline-block'}),
-                html.P(total_sales, style={'textAlign':'center', "margin-bottom":"0px", 'color': "darkgreen", 'fontSize': '40px', 'width': '100%'})
+                html.P(total_sales, style={'textAlign':'center', "margin-bottom":"0px", "margin-top":"8px", 'color': "darkgreen", 'fontSize': '45px', 'width': '100%'})
             ],style={'width':'23%','display': 'inline-block','background': 'wheat','textAlign':'center', 
                     'border-style':'solid','border-color':'wheat', 'border-radius':'20px','border-width':'5px',
-                    'margin-right':'15px','margin-bottom':'15px','margin-top':'15px','margin-left':'15px'})
-            ],style={'width':'66%','display': 'inline-block','background': 'whitesmoke'}),
-        
+                    'margin-right':'15px','margin-bottom':'15px','margin-top':'15px','margin-left':'15px', 'height':'70%'})
+            ],style={'width':'100%','display': 'inline-block','background': 'whitesmoke', 'height':'200px'}),
+        ],style={'width':'66%','display': 'inline-block','vertical-align': 'top'}),
         # # Total Right Side
-        # html.Div(children=[
-        #     html.H3("Total Sales", style={'textAlign':'center', "margin-bottom":"0px", 'color': "goldenrod", 'fontSize': '30px', 'width': '100%', 'display': 'inline-block'}),
-        #     html.P(total_sales, style={'textAlign':'center', "margin-bottom":"0px", 'color': "darkseagreen", 'fontSize': '40px', 'width': '100%'})
-        #     ],style={'width':'30%','display': 'inline-block','background': 'goldenrod','textAlign':'center', 
-        #             'border-style':'solid','border-color':'wheat', 'border-radius':'20px','border-width':'5px',
-        #             'margin-right':'15px','margin-bottom':'15px','margin-top':'15px','margin-left':'15px'})
-            ],style={'width':'100%','display': 'inline-block','background': 'grey',}),
+        html.Div(children=[
+            dcc.Graph(figure=number_fig,style={'width': '100%', 'display': 'inline-block','vertical-align': 'top'})
+            ],style={'width':'33%','display': 'inline-block'}
+            )],style={'width':'100%','display': 'inline-block','background': 'whitesmoke','vertical-align': 'top'}),
 
     # CHART DIV - LEFT
     html.Div(children=[
         dcc.Graph(figure=sales_income_fig,style={'width': '66%', 'display': 'inline-block'}),
-        dcc.Graph(figure=number_fig,style={'width': '33%', 'display': 'inline-block'})
+        dcc.Graph(figure=bubble_chart_fig,style={'width': '33%', 'display': 'inline-block'})
     ]),
 
     # html.Div(
@@ -293,7 +342,6 @@ app.layout = html.Div(children=[
     Input('my_dropdown', 'value')
 )
 def update_output(my_dropdown):
-    # return 'You have selected "{}"'.format(value)
     chart_df = data
     bins = [1939, 1965, 1980, 1996]
     name = ['Boomers', 'Gen x', 'Millennials']
